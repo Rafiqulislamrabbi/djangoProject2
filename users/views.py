060bbaacf863f9f7f7ad2bce567b108cd885aa83
ubuntu <rafiqul15-2546@diu.edu.bd>
2024-11-27 20:11:03 +0600

    all commit

--- users/views.py
+++ users/views.py
@@ -7,10 +7,7 @@ from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import UserProfile, Wallet
 from .serializers import UserSerializer, UserProfileSerializer, WalletSerializer
-
-# User Registration View
 from rest_framework.views import APIView
-from rest_framework import serializers
 
 class UserRegistrationAPIView(APIView):
     def post(self, request):
